94ffdff456a714a27aa76860c1dfc69e234a4073
jared james <jaredjames.personal@gmail.com>
2021-05-09 10:56:26 +1200

    responsive mobile

--- components/CryptoCoins/CryptoCoinsComponent.js
+++ components/CryptoCoins/CryptoCoinsComponent.js
@@ -15,40 +15,41 @@ const CryptoCoinsComponent = ({
     <Link href="/coin/[id]" as={`/coin/${id}`}>
       <a>
         <div className={styles.coin_container}>
-            <div className={styles.coin_row}>
-                <img src={image} alt={name} className={styles.coin_img} />
-                <h1 className={styles.coin_h1}>{name}</h1>
-          
-                <p className={styles.coin_symbol}>{symbol}</p>
-             
-                  <p className={styles.coin_price}>${current_price}</p>
-                  <p className={styles.coin_volume}>
-                    ${total_volume.toLocaleString()}
-                  </p>
+          <div className={styles.coin_row}>
+            <img src={image} alt={name} className={styles.coin_img} />
 
-                  {price_change_percentage_24h < 0 ? (
-                    <p className={(styles.coin_percent, styles.red)}>
-                      {price_change_percentage_24h.toFixed(2)}%
-                    </p>
-                  ) : (
-                    <p className={(styles.coin_percent, styles.green)}>
-                      {price_change_percentage_24h.toFixed(2)}%
-                    </p>
-                  )}
+            <h1 className={styles.coin_h1}>{name}</h1>
 
-                  <p className={styles.coin_marketcap}>
-                    Mkt Cap: ${market_cap.toLocaleString()}
-                  </p>
-             
-              </div>
-            </div>
+            <p className={styles.coin_symbol}>{symbol}</p>
+
+            <p className={styles.coin_price}>${current_price}</p>
+
+            {price_change_percentage_24h < 0 ? (
+              <p className={(styles.coin_percent, styles.red)}>
+                {price_change_percentage_24h.toFixed(2)}%
+              </p>
+            ) : (
+              <p className={(styles.coin_percent, styles.green)}>
+                {price_change_percentage_24h.toFixed(2)}%
+              </p>
+            )}
+
+            <p className={styles.coin_market}>
+              Market Cap: <br />
+              ${market_cap.toLocaleString()}
+            </p>
+
+            <p className={styles.coin_volume}>
+              Total Volume: ${total_volume.toLocaleString()}
+            </p>
+          </div>
+        </div>
       </a>
     </Link>
   );
 };
 
 export default CryptoCoinsComponent;
-
 
 // <div className={styles.coin_container}>
 // <div className={styles.coin_row}>
