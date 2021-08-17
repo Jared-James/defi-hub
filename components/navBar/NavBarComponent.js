428c9314ad3233fcbb1fbe1ea22bb782ba09420c
jared james <jaredjames.personal@gmail.com>
2021-08-18 09:30:05 +1200

    added styled components to nextjs

--- components/navBar/NavBarComponent.js
+++ components/navBar/NavBarComponent.js
@@ -1,17 +1,30 @@
 import React, {useState} from "react";
 import styles from "./NavBar.module.css";
 import { FaBitcoin } from "react-icons/fa";
+import styled from 'styled-components'
 
+
+const Nav = styled.div`
+  display: flex;
+  box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset,
+    rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
+  justify-content: flex-start;
+  align-items: center;
+  height: 4rem;
+  border-bottom: 0.5px solid #000;
+  background: #000046;
+  background: -webkit-linear-gradient(to right, #1cb5e0, #000046);
+  background: linear-gradient(to right, #1cb5e0, #000046);
+`
 
 const navBarComponent = () => {
 const [signIn, setSignIn] = useState(false)
-
 const handleSignIn = () => setSignIn(!signIn)
 
 
 
   return (
-    <div className={styles.nav}>
+    <Nav>
       <div className={styles.nav__icon}>
         <FaBitcoin />
       </div>
@@ -24,7 +37,7 @@ const handleSignIn = () => setSignIn(!signIn)
     
         </ul>
       </div>
-    </div>
+    </Nav>
   );
 };
 
