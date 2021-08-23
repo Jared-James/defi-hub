60bb06b2e05d6541bbca9be55c73f44dfd05fff9
jared james <jaredjames.personal@gmail.com>
2021-08-23 13:02:59 +1200

    working hotjar

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -8,26 +8,6 @@ const Layout = ({ children, title = "Crypto Tracker" }) => {
           <title>{title}</title>
           <meta name="description" />
           <link rel="icon" href="/favicon.ico" />
-          <script>
-            {(function (h, o, t, j, a, r) {
-              h.hj =
-                h.hj ||
-                function () {
-                  (h.hj.q = h.hj.q || []).push(arguments);
-                };
-              h._hjSettings = { hjid: 2564929, hjsv: 6 };
-              a = o.getElementsByTagName("head")[0];
-              r = o.createElement("script");
-              r.async = 1;
-              r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(
-              window,
-              document,
-              "https://static.hotjar.com/c/hotjar-",
-              ".js?sv="
-            )}
-          </script>
         </Head>
         <NavBarComponent />
         <main>{children}</main>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
 import { useEffect } from "react";
+import { hotjar } from 'react-hotjar';
+
+useEffect(() => {
+  hotjar.initialize(2566228, 6);
+}, [])
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
