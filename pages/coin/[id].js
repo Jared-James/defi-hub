deac65156a57b5e5b0d3e4f70fb70cddbaca333f
jared james <jaredjames.personal@gmail.com>
2021-09-21 10:33:10 +1200

    added g tag

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -1,8 +1,8 @@
-import Layout from "../../components/layout/Layout";
-import IndividualHeader from "../../components/individualCoins/IndividualHeader";
-import IndividualBody from "../../components/individualCoins/IndividualBody";
-import IndividualMetrics from "../../components/individualCoins/IndividualMetrics";
-import IndividualChart from "../../components/individualCoins/IndividualChart";
+import Layout from "../../components/layout/Layout"
+import IndividualHeader from "../../components/individualCoins/IndividualHeader"
+import IndividualBody from "../../components/individualCoins/IndividualBody"
+import IndividualMetrics from "../../components/individualCoins/IndividualMetrics"
+import IndividualChart from "../../components/individualCoins/IndividualChart"
 
 const Coin = ({ coin }) => {
   return (
@@ -14,19 +14,30 @@ const Coin = ({ coin }) => {
         <IndividualChart coin={coin} />
       </Layout>
     </>
-  );
-};
+  )
+}
 
-export default Coin;
+export default Coin
 
 export async function getServerSideProps(context) {
-  const { id } = context.query;
-  const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
-  const data = await res.json();
+  const { id } = context.query
+  const initTime = date.now()
+  const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+  const finalTime = initTime - date.now()
+  gtag.event({
+    action: "Time",
+    value: finalTime
+  })
+
+  gtag.event({
+    action: "second event",
+    value: 123
+  })
+  const data = await res.json()
 
   return {
     props: {
-      coin: data,
-    },
-  };
+      coin: data
+    }
+  }
 }
