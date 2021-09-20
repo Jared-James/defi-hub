3eda696f682e399860c15204a566c5bec3ba8c65
jared james <jaredjames.personal@gmail.com>
2021-09-21 11:08:05 +1200

    time taken added

--- components/individualCoins/IndividualChart.js
+++ components/individualCoins/IndividualChart.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 
-import styled from "styled-components";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import HighchartsExporting from "highcharts/modules/exporting";
-import moment from "moment";
-
+import styled from "styled-components"
+import Highcharts from "highcharts"
+import HighchartsReact from "highcharts-react-official"
+import HighchartsExporting from "highcharts/modules/exporting"
+import moment from "moment"
+import * as gtag from "../../gtag"
 const Container = styled.div`
   width: 100%;
   background: #fff;
-`;
+`
 
 const ChartContainer = styled.div`
   display: flex;
@@ -21,69 +21,84 @@ const ChartContainer = styled.div`
   max-width: 800px;
   background: #f8f8f8;
   padding: 2rem;
-`;
+`
 
 const Chart = styled.div`
   width: 100%;
-`;
+`
 
 const IndividualChart = ({ coin }) => {
-  const { id } = coin;
+  const { id } = coin
   const [chartOptions, setChartOptions] = useState({
     chart: {
-      type: "line",
+      type: "line"
     },
     title: {
-      text: `${id} price`.toUpperCase(),
+      text: `${id} price`.toUpperCase()
     },
     xAxis: {
       type: "datetime",
       title: {
-        text: "Date",
-      },
+        text: "Date"
+      }
     },
     yAxis: {
       title: {
-        text: `price`.toUpperCase(),
-      },
+        text: `price`.toUpperCase()
+      }
     },
     series: [
       {
         name: `${id}`,
-        data: [],
-      },
+        data: []
+      }
     ],
     plotOptions: {
       series: {
         label: {
-          connectorAllowed: false,
+          connectorAllowed: false
         },
-        pointStart: 2010,
-      },
-    },
-  });
+        pointStart: 2010
+      }
+    }
+  })
 
   const fetchData = async () => {
-    const url = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=nzd&days=180&interval=hourly`;
-    const response = await fetch(url);
-    const JSON = await response.json();
+    const initTime = Date.now()
+    const url = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=nzd&days=180&interval=hourly`
+    const response = await fetch(url)
+    const JSON = await response.json()
+    const finalTime = initTime - Date.now()
 
+    gtag.event({
+      action: "first event",
+      event_category: "this one is time",
+      event_label: "time tabel",
+      value: finalTime
+    })
+
+    gtag.event({
+      action: "second event",
+      event_category: "this ons is just random",
+      event_label: "random label",
+      value: 12345
+    })
     const mappedJSON = JSON.prices.map((item) => {
-      const styledPrice = item[1].toFixed(2);
-      const Time = item[0];
-      return [Time, Number(styledPrice)];
-    });
+      const styledPrice = item[1].toFixed(2)
+      const Time = item[0]
+      return [Time, Number(styledPrice)]
+    })
     setChartOptions({
-      series: [{ data: [...mappedJSON] }],
-    });
-  };
+      series: [{ data: [...mappedJSON] }]
+    })
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   if (typeof Highcharts === "object") {
-    HighchartsExporting(Highcharts);
+    HighchartsExporting(Highcharts)
   }
 
   return (
@@ -94,22 +109,22 @@ const IndividualChart = ({ coin }) => {
         </Chart>
       </ChartContainer>
     </Container>
-  );
-};
+  )
+}
 
-export default IndividualChart;
+export default IndividualChart
 
 export async function getServerSideProps(context) {
-  const { id } = context.query;
+  const { id } = context.query
 
   const res = await fetch(
     `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=nzd&days=180&interval=hourly`
-  );
-  const data = await res.json();
+  )
+  const data = await res.json()
 
   return {
     props: {
-      coin: data,
-    },
-  };
+      coin: data
+    }
+  }
 }
--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -22,22 +22,9 @@ export default Coin
 
 export async function getServerSideProps(context) {
   const { id } = context.query
-  const initTime = Date.now()
-  const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
-  const finalTime = initTime - Date.now()
-  gtag.event({
-    action: "Time",
-    event_category: "this one is time",
-    event_label: "time tabel",
-    value: finalTime
-  })
 
-  gtag.event({
-    action: "second event",
-    event_category: "this ons is just random",
-    event_label: "random label",
-    value: 12345
-  })
+  const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+
   const data = await res.json()
 
   return {
