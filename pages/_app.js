abf47cd079f97ddd97c1c0e993d5dcb74915a583
jared james <jaredjames.personal@gmail.com>
2021-09-21 10:22:35 +1200

    adding ga analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
-import "../styles/globals.css";
-import { useEffect } from "react";
-import { hotjar } from "react-hotjar";
+import "../styles/globals.css"
+import { useEffect } from "react"
+import { hotjar } from "react-hotjar"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    hotjar.initialize(2566228, 6);
-  }, []);
-  return <Component {...pageProps} />;
+    hotjar.initialize(2566228, 6)
+  }, [])
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-K7FVL440HM`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K7FVL440HM');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
